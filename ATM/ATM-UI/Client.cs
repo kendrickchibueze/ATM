ed922abdfd563b3a365c9d7ca7ad3113c9b0e988
kendrick chukwuka <kendrickchibueze@gmail.com>
2023-03-07 15:47:48 +0100

    code cleaning

--- ATM/ATM-UI/Client.cs
+++ ATM/ATM-UI/Client.cs
@@ -28,14 +28,15 @@ namespace ATM.ATM_UI
         {
             Service atmService = new Service();
 
-           input: Console.WriteLine("Enter your card number: ");
+           input: Utility.PrintColorMessage(ConsoleColor.Cyan, "Enter your card number: ");
             try
             {
 
             
                 Int64 cardNumber = Int64.Parse(Console.ReadLine());
 
-                Console.WriteLine("Enter your PIN code: ");
+                Utility.PrintColorMessage(ConsoleColor.Cyan, "Enter your PIN code: ");
+
                 int pinCode = Int32.Parse(Console.ReadLine());
 
                 try
@@ -44,7 +45,7 @@ namespace ATM.ATM_UI
                     {
                         await atmService.SeedDataAsync();
 
-                        Utility.PrintColorMessage(ConsoleColor.Cyan, "Authenticating user...Please wait...");
+                        Utility.PrintColorMessage(ConsoleColor.Cyan, "\n\nAuthenticating user...Please wait...");
 
                         Utility.printDotAnimation(15);
 
@@ -74,63 +75,65 @@ namespace ATM.ATM_UI
                                 {
                                     case 1:
                                         decimal balance = await atmService.CheckBalanceAsync(cardNumber, pinCode);
-                                        Console.WriteLine($"Your current balance is: {FormatAmount(balance)}");
+                                        Utility.PrintColorMessage(ConsoleColor.Green, $"Your current balance is: {FormatAmount(balance)}");
                                         await Task.Delay(3000);
                                         break;
 
                                     case 2:
-                                    inputdepo: Console.WriteLine("Enter the amount to deposit: ");
+                                    inputdepo: Utility.PrintColorMessage(ConsoleColor.Cyan, "Enter the amount to deposit: ");
                                         decimal depositAmount = decimal.Parse(Console.ReadLine());
                                         if (depositAmount < 0)
                                         {
-                                            Console.WriteLine("Error: Cannot deposit a negative amount.");
+                                            Utility.PrintColorMessage(ConsoleColor.Red, "Error: Cannot deposit a negative amount.");
                                             goto inputdepo;
                                         }
                                         bool depositResult = await atmService.DepositAsync(cardNumber, pinCode, depositAmount);
                                         if (depositResult)
                                         {
-                                            Console.WriteLine("Deposit successful.");
+                                            Utility.PrintColorMessage(ConsoleColor.Green, "Deposit successful.");
                                         }
                                         else
                                         {
-                                            Console.WriteLine("Error: Deposit failed.");
+                                            Utility.PrintColorMessage(ConsoleColor.Red, "Error: Deposit failed.");
                                         }
 
                                         await Task.Delay(3000);
                                         break;
 
                                     case 3:
-                                    inputwithdraw: Console.WriteLine("Enter the amount to withdraw: ");
+                                    inputwithdraw: Utility.PrintColorMessage(ConsoleColor.Cyan, "Enter the amount to withdraw: ");
+
                                         decimal withdrawAmount = decimal.Parse(Console.ReadLine());
+
                                         if (withdrawAmount < 0)
                                         {
-                                            Console.WriteLine("Error: Cannot withdraw a negative amount.");
+                                            Utility.PrintColorMessage(ConsoleColor.Red, "Error: Cannot withdraw a negative amount.");
                                             goto inputwithdraw;
                                         }
                                         bool withdrawResult = await atmService.WithdrawAsync(cardNumber, pinCode, withdrawAmount);
                                         if (withdrawResult)
                                         {
-                                            Console.WriteLine("Withdrawal successful.");
+                                            Utility.PrintColorMessage(ConsoleColor.Green, "Withdrawal successful.");
                                         }
                                         else
                                         {
-                                            Console.WriteLine("Error: Withdrawal failed.");
+                                            Utility.PrintColorMessage(ConsoleColor.Red, "Error: Withdrawal failed.");
                                         }
                                         await Task.Delay(3000);
                                         break;
 
                                     case 4:
-                                    intransfer: Console.WriteLine("Enter the recipient's Account number: ");
+                                    intransfer: Utility.PrintColorMessage(ConsoleColor.Cyan, "Enter the recipient's Account number: ");
 
                                         Int64 recipientAccountNumber = Int64.Parse(Console.ReadLine());
 
-                                        Console.WriteLine("Enter the amount to transfer: ");
+                                        Utility.PrintColorMessage(ConsoleColor.Cyan, "Enter the amount to transfer: ");
 
                                         decimal transferAmount = decimal.Parse(Console.ReadLine());
 
                                         if (transferAmount < 0)
                                         {
-                                            Console.WriteLine("Error: Cannot transfer a negative amount.");
+                                            Utility.PrintColorMessage(ConsoleColor.Red, "Error: Cannot transfer a negative amount.");
                                             goto intransfer;
                                         }
 
@@ -143,45 +146,48 @@ namespace ATM.ATM_UI
                                         bool transferResult = await atmService.TransferAsync(cardNumber, pinCode, transfer);
                                         if (transferResult)
                                         {
-                                            Console.WriteLine("Transfer successful.");
+                                            Utility.PrintColorMessage(ConsoleColor.Green, "Transfer successful.");
                                         }
                                         else
                                         {
-                                            Console.WriteLine("Error: Transfer failed.");
+                                            Utility.PrintColorMessage(ConsoleColor.Red, "Error: Transfer failed.");
                                         }
 
-                                        await Task.Delay(3000);
+                                        await Task.Delay(6000);
                                         break;
 
                                     case 5:
                                         var transactions = await atmService.ViewAllTransactionsAsync(cardNumber, pinCode);
 
-                                        Console.WriteLine("Transactions:");
+                                        Utility.PrintColorMessage(ConsoleColor.Cyan, "Transactions:");
 
                                         foreach (var transaction in transactions)
                                         {
-                                            Console.WriteLine($"- Type: {transaction.TransactionType}, Amount: {FormatAmount(transaction.TransactionAmount)}, Date: {transaction.TransactionDate}");
+                                            Utility.PrintColorMessage(ConsoleColor.Green, $"- Type: {transaction.TransactionType}, Amount: {FormatAmount(transaction.TransactionAmount)}, Date: {transaction.TransactionDate}");
                                         }
-                                        await Task.Delay(3000);
+                                        await Task.Delay(4000);
+
+                                        
+
                                         break;
 
                                     case 6:
-                                        Console.WriteLine("Logout successful.");
+                                        Utility.PrintColorMessage(ConsoleColor.Green, "Logout successful.");
 
                                         Environment.Exit(0);
                                         break;
 
                                     default:
-                                        Console.WriteLine("Error: Invalid operation.");
+                                        Utility.PrintColorMessage(ConsoleColor.Red, "Error: Invalid operation.");
                                         goto input;
                                         break;
                                 }
                             }
                             catch (Exception ex)
                             {
-                                Console.WriteLine($"Error: {ex.Message}");
+                                Utility.PrintColorMessage(ConsoleColor.Red, $"Error: {ex.Message}");
                                 goto input;
-                                //continue;
+                                
                             }
 
                             Console.WriteLine();
@@ -190,7 +196,7 @@ namespace ATM.ATM_UI
                 }
                 catch (Exception ex)
                 {
-                    Console.WriteLine($"Error: {ex.Message}");
+                    Utility.PrintColorMessage(ConsoleColor.Red, $"Error: {ex.Message}");
                     goto input;
                     return;
                 }
@@ -198,7 +204,7 @@ namespace ATM.ATM_UI
             }
             catch(Exception ex)
             {
-                Console.WriteLine($"Error: {ex.Message}");
+                Utility.PrintColorMessage(ConsoleColor.Red, $"Error: {ex.Message}");
                 goto input;
             }
 
